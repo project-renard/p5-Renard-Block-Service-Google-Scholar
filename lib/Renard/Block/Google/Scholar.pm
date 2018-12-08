b51a1a7165437143195a7d603b2a3ecce382c8ca
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-12-07 23:03:14 -0600

    Remove ::Incunabula:: from namespace

---
+++ lib/Renard/Block/Google/Scholar.pm
@@ -0,0 +1,13 @@
+package Renard::Block::Google::Scholar;
+
+use strict;
+use warnings;
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Block-Google-Scholar/>
+
+=cut
